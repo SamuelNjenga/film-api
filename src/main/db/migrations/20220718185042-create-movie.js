f8e0001066b2d7d6ab83a1da85392122c2207e63
Samuel Njenga <snjenga704@gmail.com>
2022-07-18 22:03:31 +0300

    feat: initial commit

---
+++ src/main/db/migrations/20220718185042-create-movie.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Movies', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      rating: {
+        type: Sequelize.INTEGER
+      },
+      language: {
+        type: Sequelize.STRING
+      },
+      genreId: {
+        type: Sequelize.INTEGER
+      },
+      releaseDate: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Movies');
+  }
+};
