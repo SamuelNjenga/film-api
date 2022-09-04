6a0d930a5a5375e295290419e8d345e3071d0714
Samuel Njenga <snjenga704@gmail.com>
2022-09-04 11:20:38 +0300

    feat: add getPagination and getPagingData functions in MovieService.js

--- src/main/services/MovieService.js
+++ src/main/services/MovieService.js
@@ -16,6 +16,21 @@ exports.getMovie = async (data) => {
   return db.Movie.findByPk(data);
 };
 
+exports.getPagination = (page, size) => {
+  const limit = size ? +size : 5;
+  const offset = page ? page * limit : 0;
+
+  return { limit, offset };
+};
+
+exports.getPagingData = (data, page, limit) => {
+  const { count: totalMovies, rows: movies } = data;
+  const currentPage = page ? +page : 0;
+  const totalPages = Math.ceil(totalActors / limit);
+
+  return { totalMovies, movies, totalPages, currentPage };
+};
+
 exports.deleteMovie = async (data) => {
   return db.Movie.destroy(data);
 };
